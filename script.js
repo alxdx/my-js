9aa3c1d560a812526022afd65785ecf98e742d99
alxdx <ustongs09@gmail.com>
2021-11-05 11:50:55 -0600

    .

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	var table = $('#datatable').DataTable({
 		columnDefs:[
 			{
@@ -7,7 +8,29 @@ $(document).ready(function(){
 			}
 		],
         buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
+            'copy', 'excel', 
+            {
+            	extend:'pdf',
+            	text:'Guardar pagina actual en PDF',
+                orientation:'landscape',
+            	exportOptions: {
+	                modifier: {
+	                    page: 'current'
+	                },
+	                columns:[0,1,2,3,4,5,6,10,11,12,7]
+            	}
+            },
+            {
+            	extend:'print',
+            	text:'Imprimir pagina actual',
+                orientation:'landscape',
+            	exportOptions: {
+	                modifier: {
+	                    page: 'current'
+	                },
+	                columns:[0,1,2,3,4,5,6,10,11,12]
+            	}
+            }
         ],
 		searching:true,
 		paging:true,
@@ -15,16 +38,33 @@ $(document).ready(function(){
 		lengthChange:false,
 		autoWidth:false,
 		info:true,
-		select:true,
-		scrollCollapse:true,
-		fixedHeader:false,
 		responsive:true,
-		fixedHeader:true,
 		colReorder:{
-			order:[0,1,2,3,4,5,6,10,11,12,13,14,15,16,17,7,8,9]
+			order:[0,1,2,3,4,5,6,10,11,12,7,14,15,16,17,13,8,9]
 		},
-		rowGroup:false
+		rowGroup:false,
+		language:{
+			search:"Buscar en toda la tabla:",
+			paginate:{
+				"previous":"Anterior",
+				"next":"Siguiente"
+			},
+			buttons:{
+				"copy":"Copiar",
+				"print":"Imprimir"
+			},
+			"info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+			"infoFiltered": "(Filtrado de _MAX_ registros)",
+			"infoEmpty": "Mostrando 0 a 0 de 0 registros",
+			"zeroRecords": "No se encontraron coincidencias"
+		}
 	});
-table.buttons().container()
-        .appendTo( '#datatable_wrapper .col-sm-6:eq(0)' );
+	table.buttons().container().appendTo( '#datatable_wrapper .col-sm-6:eq(0)' );
+    new $.fn.dataTable.FixedHeader( table );
+    
 });
+
+
+
+
+
