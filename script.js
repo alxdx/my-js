5898469db316fe4af037b255a91ae7d0e76c5665
alxdx <ustongs09@gmail.com>
2021-11-08 08:53:25 -0600

    added column searchers, style modified, integrates columns reorder, this version is being delivered

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 $(document).ready(function(){
 
-	var table = $('#datatable').DataTable({
+    //$('#datatable thead tr td').each(function(){
+    //	console.log(this);
+    //	$(this).append( '<input type="text" placeholder="Search '+'" />' );
+    //});
+    $('#datatable thead tr')
+        .clone(true)
+        .appendTo('#datatable thead');
+
+	var table = $('#datatable').
+        on('init.dt',function(){
+            var api = $('#datatable').dataTable().api();
+            api.columns().eq(0).each(function (colIdx) {
+                // Set the header cell to contain the input element
+                var cell = $('#datatable thead td').eq(
+                     $(api.column(colIdx).header()).index()
+                );
+                var title = $(cell).text();
+                if(title!='Cant'){
+                    $(cell).html('<input type="text" placeholder="' + title + '" class="hidInput" />');
+                }else{
+                    $(cell).html('');                    
+                }
+                if(!api.column(colIdx).responsiveHidden()){
+                    cell.addClass('hide');
+                }
+            });
+        })
+        .DataTable({
 		columnDefs:[
 			{
 				targets:[8,9],
@@ -34,15 +61,19 @@ $(document).ready(function(){
         ],
 		searching:true,
 		paging:true,
-		pageLength:50,
+		pageLength:200,
 		lengthChange:false,
 		autoWidth:false,
 		info:true,
-		responsive:true,
+        responsive:true,
 		colReorder:{
 			order:[0,1,2,3,4,5,6,10,11,12,7,14,15,16,17,13,8,9]
 		},
-		rowGroup:false,
+		rowGroup: false
+        //{
+		//	dataSrc:[0,1]
+		//}
+        ,
 		language:{
 			search:"Buscar en toda la tabla:",
 			paginate:{
@@ -62,9 +93,90 @@ $(document).ready(function(){
 	table.buttons().container().appendTo( '#datatable_wrapper .col-sm-6:eq(0)' );
     new $.fn.dataTable.FixedHeader( table );
     
+    $("#datatable thead td input").on( 'keyup change', function () {
+    	console.log($(this).attr('name'));
+        table.column( $(this).parent().index()+':visible' ).search( this.value ).draw();
+    } );
+
+    table.on('responsive-resize', function ( e, api, columns ) {
+        api.columns().eq(0).each(function (colIdx) {
+                var cell = $('#datatable thead td').eq(
+                     $(api.column(colIdx).header()).index()
+                );
+                if(columns[colIdx]){
+                    cell.removeClass('hide');
+                }else{
+                    cell.addClass('hide');
+                }
+            });
+    });
 });
 
 
 
 
+/**
+$(document).ready(function () {
+    // Setup - add a text input to each footer cell
+    $('#example thead tr')
+        .clone(true)
+        .addClass('filters')
+        .appendTo('#example thead');
+ 
+    var table = $('#example').DataTable({
+        orderCellsTop: true,
+        fixedHeader: true,
+        initComplete: function () {
+            var api = this.api();
+ 
+            // For each column
+            api
+                .columns()
+                .eq(0)
+                .each(function (colIdx) {
+                    // Set the header cell to contain the input element
+                    var cell = $('.filters th').eq(
+                        $(api.column(colIdx).header()).index()
+                    );
+                    var title = $(cell).text();
+                    $(cell).html('<input type="text" placeholder="' + title + '" />');
+ 
+                    // On every keypress in this input
+                    $(
+                        'input',
+                        $('.filters th').eq($(api.column(colIdx).header()).index())
+                    )
+                        .off('keyup change')
+                        .on('keyup change', function (e) {
+                            e.stopPropagation();
+ 
+                            // Get the search value
+                            $(this).attr('title', $(this).val());
+                            var regexr = '({search})'; //$(this).parents('th').find('select').val();
+ 
+                            var cursorPosition = this.selectionStart;
+                            // Search the column for that value
+                            api
+                                .column(colIdx)
+                                .search(
+                                    this.value != ''
+                                        ? regexr.replace('{search}', '(((' + this.value + ')))')
+                                        : '',
+                                    this.value != '',
+                                    this.value == ''
+                                )
+                                .draw();
+ 
+                            $(this)
+                                .focus()[0]
+                                .setSelectionRange(cursorPosition, cursorPosition);
+                        });
+                });
+        },
+    });
+});
+
+
+
+**/
 
